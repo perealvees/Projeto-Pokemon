69de10f00d4fc7c650b22cee372aea59de335c50
Priscilla Alves <perealvees@gmail.com>
2023-05-17 13:18:42 -0300

    adicionando ações JS

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,2 +1,16 @@
-//alert('BEm vindo! Escolha seu companheiro de batalha:')
-const botaoAlterarTema =  document.getElementById("botao-alterar-tema");
+alert('BEm vindo! Escolha seu companheiro de batalha:')
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+  body.classList.toggle("modo-escuro");
+
+  if (modoEscuroEstaAtivo) {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+  } else {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+  }
+});
